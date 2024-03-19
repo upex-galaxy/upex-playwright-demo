4ba6845bd4672a4af3b707d08f8d2633658a4048
inakibustos <inaki.bustosfunes@gmail.com>
2024-03-19 20:24:14 -0300

    Segundo test TC2: Right Click button

--- tests/specs/GX3-2831_Elementos_Buttons.spec.ts
+++ tests/specs/GX3-2831_Elementos_Buttons.spec.ts
@@ -15,4 +15,18 @@ test.describe('GX3-2831',()=>{
 			await expect(page.getByText("You have done a double click")).toBeVisible();
 	})
 	});
+
+	test('TC2:Validar hacer click derecho en el boton',async({page})=>{
+		await test.step('tc2',async()=>{
+			await page.goto('https://demoqa.com/buttons');
+		});
+
+		await test.step('Deberia visualizarse el mensaje "You have done a right click"',async()=>{
+			await page.getByText("Right Click Me").click({ button: 'right' });
+			//validacion
+			await expect(page.getByText("You have done a right click")).toBeVisible();
+	})
+	});
+
+	
 });
