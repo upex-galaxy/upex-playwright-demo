9036300b8558625972d102b28011b7d48742c656
inakibustos <inaki.bustosfunes@gmail.com>
2024-03-20 13:01:38 -0300

    GX3-2831 Correccion beforeEach Primer test TC1:double click button

--- tests/specs/GX3-2831_Elementos_Buttons.spec.ts
+++ tests/specs/GX3-2831_Elementos_Buttons.spec.ts
@@ -2,12 +2,14 @@ import {test,expect,Page,} from '@playwright/test';
 let page:Page;
 
 
-test.describe('GX3-2831',()=>{
+test.describe('GX3-2831 | TS: ⚡️ToolsQA | Elements | Buttons',()=>{
+	test.beforeEach(async({page})=>{
+		await page.goto('https://demoqa.com/buttons');
+	});
+	
 	
 	test('TC1:Validar hacer doble click en el boton',async({page})=>{
-		await test.step('GX3-2831| TC1: Validar mostrar mensaje en pantalla cuando hago click en el botón "Double Click Me"',async()=>{
-			await page.goto('https://demoqa.com/buttons');
-		});
+		
 
 		await test.step('Deberia visualizarse el mensaje "You have done a double click"',async()=>{
 			await page.getByText("Double Click Me").dblclick();
@@ -16,7 +18,7 @@ test.describe('GX3-2831',()=>{
 	})
 	});
 
-	test('TC2:Validar hacer click derecho en el boton',async({page})=>{
+	/*test('TC2:Validar hacer click derecho en el boton',async({page})=>{
 		await test.step('tc2',async()=>{
 			await page.goto('https://demoqa.com/buttons');
 		});
@@ -39,4 +41,5 @@ test.describe('GX3-2831',()=>{
 			await expect(page.getByText("You have done a dynamic click")).toBeVisible();
 	})
 	});
+	*/
 });
