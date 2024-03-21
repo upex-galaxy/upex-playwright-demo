f596d4b8b3c62f3c47a5e0a04ff3f468ad1b5ea9
inakibustos <inaki.bustosfunes@gmail.com>
2024-03-21 12:15:08 -0300

    GX3-2854 Se agrego un TC2 y se corrigieron errores del TC1

--- tests/specs/GX3-2854_Elementos_Fill_Form_and_Submit.spec.ts
+++ tests/specs/GX3-2854_Elementos_Fill_Form_and_Submit.spec.ts
@@ -8,12 +8,12 @@ test.describe('GX3-2854 ',()=>{
 		await page.goto('https://demoqa.com/text-box');
 	});
 
-	test('TC1',async({page})=>{
+	test('TC1 Validar completar el formulario',async({page})=>{
 		const usernameInput = page.locator('#userName-wrapper input');
 		const emailInput= page.locator('#userEmail-wrapper input');
 		const currentAdInput= page.locator('#currentAddress-wrapper textarea');
 		const permanentAdInput= page.locator('#permanentAddress-wrapper textarea');
-		const submitButton= page.locator('button',{hasText:'Submit'});
+		
 		
 		
 		await test.step('deberia completarse el campo fullname', async () => {
@@ -32,16 +32,51 @@ test.describe('GX3-2854 ',()=>{
 			await permanentAdInput.fill(data[0].permanentAddress);
 		});
 
+		
+
+	});
+
+	test('TC2: Validar enviar formulario', async({page})=>{
+		const submitButton= page.locator('button',{hasText:'Submit'});
+		const outputName=page.locator('#output #name');
+		const outputEmail=page.locator('#output #email');
+		const outputCurrentAd=page.locator('#output #currentAddress');
+		const outputPermanentAd=page.locator('#output #permanentAddress');
+		const usernameInput = page.locator('#userName-wrapper input');
+		const emailInput= page.locator('#userEmail-wrapper input');
+		const currentAdInput= page.locator('#currentAddress-wrapper textarea');
+		const permanentAdInput= page.locator('#permanentAddress-wrapper textarea');
+		
+		
+		
+		await test.step('deberia completarse el campo fullname', async () => {
+			await usernameInput.fill(data[0].fullName);
+		});
+
+		await test.step('debería completarse el campo email', async () => {
+			await emailInput.fill(data[0].email);
+		});
+
+		await test.step('deberia completarse el campo currentAddress', async () => {
+			await currentAdInput.fill(data[0].currentAddress);
+		});
+
+		await test.step('deberia completarse el campo permanentAddress', async () => {
+			await permanentAdInput.fill(data[0].permanentAddress);
+		});
+
+
 		await test.step('enviar formulario', async () => {
 			await submitButton.click();
 			
 		});
 
-		
-		
-		
-		
-		
+		await test.step('verificar el output', async () => {
+			expect(outputName).toContainText(data[0].fullName);
+			expect(outputEmail).toContainText(data[0].email);
+			expect(outputCurrentAd).toContainText(data[0].currentAddress);
+			expect(outputPermanentAd).toContainText(data[0].permanentAddress);
+		});
 		
 	});
 
