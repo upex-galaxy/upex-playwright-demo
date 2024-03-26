fcefd6f9a4ede4faecaeaad117f6029d501f5fb1
inakibustos <inaki.bustosfunes@gmail.com>
2024-03-26 19:05:24 -0300

    GX3-2865 commit final

--- tests/specs/GX3-2865_Tools-qa-interactions-selectable.spec.ts
+++ tests/specs/GX3-2865_Tools-qa-interactions-selectable.spec.ts
@@ -1,28 +1,44 @@
-import{test,expect,Page} from '@playwright/test';
-import {listPOM} from '../pages/iñakibustosPOM';
-//import { listPOM } from './ListPOM';
+import{expect, test,Locator} from '@playwright/test';
+import { ListPOM } from '@pages/iñakibustosPOM';
 
 
-test.describe('GX3-2865',()=>{
+
+test.describe('GX3-2865 ToolsQA | Interactions | Selectable',()=>{
 	test.beforeEach(async({page})=>{
-		await page.goto('https://demoqa.com/selectable');
-	});
-
-	test('TC1 clickear un boton de la lista', async({page})=>{
-		//const list = new listPOM(page);
-		
-		//await listPOM.locatorList;
-			
-		await listPOM.clickLocator();
-		
-			
-		
+		await page.goto("https://demoqa.com/selectable");
 		
 	});
 
+	test('GX3-2865 | TC1:Validar seleccionar elementos visualizados en List',async({page})=>{
+		const activeList: Locator= page.locator('li.list-group-item.active',{hasText:'Cras justo odio'});
+		const listPOM = new ListPOM(page)
+		
+		await listPOM.clickLocatorList();
+		await expect(activeList).toHaveClass(/active/);
 
 
+	});
 
+	test('GX3-2865 | TC2:Validar seleccionar elementos visualizados en Grid',async({page})=>{
+		const activeListGrid: Locator= page.locator('li.list-group-item.active',{hasText:'One'});		
+		const listPOM = new ListPOM(page)
+		
+		await test.step('hacer click en "Grid"', async () => {
+			await page.getByText('Grid').click();
+		});
+		
+		await listPOM.clickLocatorGrid();
 
-
+		
+		await expect(activeListGrid).toHaveClass(/active/);
+			
+	});
 });
+
+	
+
+
+
+
+
+
