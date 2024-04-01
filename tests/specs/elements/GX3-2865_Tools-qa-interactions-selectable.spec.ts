d38ed10b70e558b8ff970db9b40345b532ddf23c
inakibustos <inaki.bustosfunes@gmail.com>
2024-04-01 14:32:23 -0300

    GX3-2865 Se sustituye el locator del TC2

--- tests/specs/elements/GX3-2865_Tools-qa-interactions-selectable.spec.ts
+++ tests/specs/elements/GX3-2865_Tools-qa-interactions-selectable.spec.ts
@@ -10,7 +10,7 @@ test.describe('GX3-2865 ToolsQA | Interactions | Selectable',()=>{
 	});
 
 	test('GX3-2865 | TC1:Validar seleccionar elementos visualizados en List',async({page})=>{
-		test.setTimeout(6000);
+		
 		
 		const activeList: Locator= page.locator('li.list-group-item.active',{hasText:'Cras justo odio'});
 		const listPOM = new ListPOM(page)
@@ -22,13 +22,13 @@ test.describe('GX3-2865 ToolsQA | Interactions | Selectable',()=>{
 	});
 
 	test('GX3-2865 | TC2:Validar seleccionar elementos visualizados en Grid',async({page})=>{
-		test.setTimeout(6000);
+		
 		
 		const activeListGrid: Locator= page.locator('li.list-group-item.active',{hasText:'One'});		
 		const listPOM = new ListPOM(page)
 		
 		await test.step('hacer click en "Grid"', async () => {
-			await page.getByText('Grid').click();
+			await page.getByRole('tab',{name:'Grid'}).click();
 		});
 		
 		await listPOM.clickLocatorGrid();
