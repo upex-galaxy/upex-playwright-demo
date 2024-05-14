ee15263d506169a4e0da0390cb299908b7f3fc88
Carlos-J-Romero <carlosjromero05mail.com>
2024-05-13 22:11:12 -0300

    test(GX3-3334):Nueva prueba automatizada para elements buttons

--- tests/specs/elements/GX3-2865_Tools-qa-interactions-selectable.spec.ts
+++ tests/specs/elements/GX3-2865_Tools-qa-interactions-selectable.spec.ts
@@ -1,31 +1,27 @@
-import{expect, test,Locator} from '@playwright/test';
+import type { Locator } from '@playwright/test';
+import{ expect, test } from '@playwright/test';
 import { ListPOM } from '@pages/iñakibustosPOM';
 
-
-
-test.describe('GX3-2865 ToolsQA | Interactions | Selectable',()=>{
-	test.beforeEach(async({page})=>{
-		await page.goto("https://demoqa.com/selectable", { waitUntil: 'domcontentloaded' });
+test.describe('GX3-2865 ToolsQA | Interactions | Selectable',() => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto('https://demoqa.com/selectable', { waitUntil: 'domcontentloaded' });
 		
 	});
 
-	test('GX3-2865 | TC1:Validar seleccionar elementos visualizados en List',async({page})=>{
+	test('GX3-2865 | TC1:Validar seleccionar elementos visualizados en List',async ({ page }) => {
 		
-		
-		const activeList: Locator= page.locator('li.list-group-item.active',{hasText:'Cras justo odio'});
-		const listPOM = new ListPOM(page)
+		const activeList: Locator= page.locator('li.list-group-item.active',{ hasText:'Cras justo odio' });
+		const listPOM = new ListPOM(page);
 		
 		await listPOM.clickLocatorList();
 		await expect(activeList).toHaveClass(/active/);
-
 		
 	});
 
-	test('GX3-2865 | TC2:Validar seleccionar elementos visualizados en Grid',async({page})=>{
+	test('GX3-2865 | TC2:Validar seleccionar elementos visualizados en Grid',async ({ page }) => {
 		
-		
-		const activeListGrid: Locator= page.locator('li.list-group-item.active',{hasText:'One'});		
-		const listPOM = new ListPOM(page)
+		const activeListGrid: Locator= page.locator('li.list-group-item.active',{ hasText:'One' });		
+		const listPOM = new ListPOM(page);
 		
 		await test.step('hacer click en "Grid"', async () => {
 			await page.locator('a').getByText('Grid').click();
@@ -37,11 +33,3 @@ test.describe('GX3-2865 ToolsQA | Interactions | Selectable',()=>{
 			
 	});
 });
-
-	
-
-
-
-
-
-
