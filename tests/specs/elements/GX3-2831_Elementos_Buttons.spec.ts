50cef6df94410c5f1ba81fbe765c2c97b51b81b9
saiotest <elyermad@gmail.com>
2024-05-22 19:27:34 -0300

    feat: added the adBlock extension to chrome context in playwright fixture. Now demoqa wont give any performance issues

--- tests/specs/elements/GX3-2831_Elementos_Buttons.spec.ts
+++ tests/specs/elements/GX3-2831_Elementos_Buttons.spec.ts
@@ -1,41 +1,35 @@
-import {test,expect,Page} from '@playwright/test';
-let page:Page;
+import { test,expect } from '@TestBase';
 
-
-test.describe('GX3-2831 | TS: ⚡️ToolsQA | Elements | Buttons',()=>{
-	test.beforeEach(async({page})=>{
+test.describe('GX3-2831 | TS: ⚡️ToolsQA | Elements | Buttons',() => {
+	test.beforeEach(async ({ page }) => {
 		await page.goto('https://demoqa.com/buttons');
 	});
 	
-	
-	test('TC1:Validar hacer doble click en el boton',async({page})=>{
-		
+	test('TC1:Validar hacer doble click en el boton',async ({ page }) => {
 
-		await test.step('Deberia visualizarse el mensaje "You have done a double click"',async()=>{
-			await page.getByText("Double Click Me").dblclick();
+		await test.step('Deberia visualizarse el mensaje "You have done a double click"',async () => {
+			await page.getByText('Double Click Me').dblclick();
 			
-			await expect(page.getByText("You have done a double click")).toBeVisible();
-	})
+			await expect(page.getByText('You have done a double click')).toBeVisible();
+		});
 	});
 
-	test('TC2:Validar hacer click derecho en el boton',async({page})=>{
-		
+	test('TC2:Validar hacer click derecho en el boton',async ({ page }) => {
 
-		await test.step('Deberia visualizarse el mensaje "You have done a right click"',async()=>{
-			await page.getByText("Right Click Me").click({ button: 'right' });
+		await test.step('Deberia visualizarse el mensaje "You have done a right click"',async () => {
+			await page.getByText('Right Click Me').click({ button: 'right' });
 			
-			await expect(page.getByText("You have done a right click")).toBeVisible();
-	})
+			await expect(page.getByText('You have done a right click')).toBeVisible();
+		});
 	});
 
-	test('TC3:Validar hacer click en el boton con id dinamico',async({page})=>{
-		
+	test('TC3:Validar hacer click en el boton con id dinamico',async ({ page }) => {
 
-		await test.step('Deberia visualizarse el mensaje "You have done a dynamic click"',async()=>{
+		await test.step('Deberia visualizarse el mensaje "You have done a dynamic click"',async () => {
 			await page.getByText('Click Me', { exact: true }).click();
 			
-			await expect(page.getByText("You have done a dynamic click")).toBeVisible();
-	})
+			await expect(page.getByText('You have done a dynamic click')).toBeVisible();
+		});
 	});
 	
 });
