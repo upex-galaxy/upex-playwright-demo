b2b20456fe021ef9cfd4baf15c28325fbf6039c9
Carlos-J-Romero <carlosjromero05mail.com>
2024-05-28 00:15:31 -0300

    fix(GX3-3378): remove describe name and add test IDs

--- tests/specs/elements/GX3-3378_ElementsRadioButtons.test.ts
+++ tests/specs/elements/GX3-3378_ElementsRadioButtons.test.ts
@@ -1,22 +1,22 @@
 import { test, expect } from '@TestBase';
-
-test.describe('TS: ToolsQA | Elements | Radio Buttons', () => {
+//* GX3-3378 | TS: ⚡️ToolsQA | Elements | Radio Buttons
+test.describe( () => {
 	test.beforeEach(async ( { page } ) => {                         
 		await page.goto('/radio-button', { waitUntil: 'domcontentloaded' });
 	});
 
-	test('TC1: Validar poder seleccionar elemento radio button "Yes".', async ({ page }) => {
+	test('3379 | TC1: Validar poder seleccionar elemento radio button "Yes".', async ({ page }) => {
 		const rBtYes = page.locator('#yesRadio');
 		await rBtYes.click({ force: true }); // Tube que forzar porque existe otro elemento que obstruye el elemento dado
 		await expect(rBtYes).toBeChecked();
 	});
 	
-	test('TC2: Validar poder seleccionar elemento radio button "Impressive".', async ({ page }) => {
+	test('3379 | TC2: Validar poder seleccionar elemento radio button "Impressive".', async ({ page }) => {
 		const rBtImprv = page.locator('#impressiveRadio');
 		await rBtImprv.click({ force: true }); // Tube que forzar porque existe otro elemento que obstruye el elemento dado
 		await expect(rBtImprv).toBeChecked();
 	});
-	test('TC3: Validar que elemento radio button "No" esté deshabilitado.', async ({ page }) => {
+	test('3379 | TC3: Validar que elemento radio button "No" esté deshabilitado.', async ({ page }) => {
 		const rBtNoRd = page.locator('#noRadio');
 		//await expect(rBtNoRd).toHaveAttribute('disabled');
 		await expect(rBtNoRd).toBeDisabled();
