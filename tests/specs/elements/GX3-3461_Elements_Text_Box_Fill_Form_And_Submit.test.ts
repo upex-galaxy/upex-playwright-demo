da3f74e8d8c924fff9689e93cef1bd0d35e18737
Carlos-J-Romero <carlosjromero05mail.com>
2024-07-04 17:10:29 -0300

    Se creo un archivo tYpo .JSON en folder /data para respaldo de 8 TCs automatizados y seleccionados a partir de una tabla de particiones equivalentes en EXCEL.

--- tests/specs/elements/GX3-3461_Elements_Text_Box_Fill_Form_And_Submit.test.ts
+++
@@ -1,6 +0,0 @@
-import { test } from '@TestBase';
-
-test.describe('TS:GX3-3334 elementsButtons', () => {
-	test.beforeEach(async ({ page }) => {
-		await page.goto('/buttons', { waitUntil: 'domcontentloaded' });
-	});
