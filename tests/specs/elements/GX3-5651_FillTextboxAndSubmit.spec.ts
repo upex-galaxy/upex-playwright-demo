91cc0a92cd3b6dda838bf096cd5b76e809daaf97
igflores <139146133+igflores@users.noreply.github.com>
2024-11-10 12:53:16 -0600

    test(GX3-5651): Updated names of fixture's data

--- tests/specs/elements/GX3-5651_FillTextboxAndSubmit.spec.ts
+++ tests/specs/elements/GX3-5651_FillTextboxAndSubmit.spec.ts
@@ -62,30 +62,42 @@ story('GX3-565 | ToolsQA | Elements | Text Box: Fill form and Submit', () => {
 
 	test('TC05: Should NOT submit the form when the Email field does not contain (minimum) 1 alphanumeric character after @', async () => {
 		await fillAndSubmitPage.fillEmail(data.invalidEmails['notCharacterAfter@']);
+		await fillAndSubmitPage.fillName(data.name['nameTC05-Alphanumeric-Space']);
+		await fillAndSubmitPage.fillPermanentAddress(data.permanentAddress['permanentAddressTC05-SpecialCharacters-Spaces']);
 		await fillAndSubmitPage.submitForm();
 		await expect(fillAndSubmitPage.outputArea()).toBeHidden();
 		await expect(fillAndSubmitPage.redBorderEmailField()).toBeVisible();
 	});
 	test('TC06: Should NOT submit the form when the Email field does not contain “.” after 1 alphanumeric character after @', async () => {
 		await fillAndSubmitPage.fillEmail(data.invalidEmails['not.Point']);
+		await fillAndSubmitPage.fillName(data.name['nameTC06-Space-Point']);
+		await fillAndSubmitPage.fillCurrentAddress(data.currentAddress['currentAddressTC06-SpecialCharacters-Spaces']);
 		await fillAndSubmitPage.submitForm();
 		await expect(fillAndSubmitPage.outputArea()).toBeHidden();
 		await expect(fillAndSubmitPage.redBorderEmailField()).toBeVisible();
 	});
 	test('TC07: Should NOT submit the form when the Email field does not contain (minimum) 2 alphanumeric characters after . the point', async () => {
 		await fillAndSubmitPage.fillEmail(data.invalidEmails['not2CharacterAfter.Point']);
+		await fillAndSubmitPage.fillName(data.name['nameTC07-1character']);
+		await fillAndSubmitPage.fillCurrentAddress(data.currentAddress['currentAddressTC07-extraSpaces']);
+		await fillAndSubmitPage.fillPermanentAddress(data.permanentAddress['permanentAddressTC07-capitalLetters']);
 		await fillAndSubmitPage.submitForm();
 		await expect(fillAndSubmitPage.outputArea()).toBeHidden();
 		await expect(fillAndSubmitPage.redBorderEmailField()).toBeVisible();
 	});
 	test('TC08: Should NOT submit the form when the Email field contains whitespace in the Email address', async () => {
 		await fillAndSubmitPage.fillEmail(data.invalidEmails['containsWhitespace']);
+		await fillAndSubmitPage.fillName(data.name['nameTC08-SpecialCharacters']);
+		await fillAndSubmitPage.fillCurrentAddress(data.currentAddress['currentAddressTC08-Points']);
+		await fillAndSubmitPage.fillPermanentAddress(data.permanentAddress['permanentAddressTC08-SpecialCharacters']);
 		await fillAndSubmitPage.submitForm();
 		await expect(fillAndSubmitPage.outputArea()).toBeHidden();
 		await expect(fillAndSubmitPage.redBorderEmailField()).toBeVisible();
 	});
 	test('TC09: Should NOT submit the form when the Email field contains additional special characters beyond @ and . the point', async () => {
 		await fillAndSubmitPage.fillEmail(data.invalidEmails['additionalSpecialCharacters']);
+		await fillAndSubmitPage.fillName(data.name['nameTC09-lowercase']);
+		await fillAndSubmitPage.fillCurrentAddress(data.currentAddress['currentAddressTC09-SpecialCharacters']);
 		await fillAndSubmitPage.submitForm();
 		await expect(fillAndSubmitPage.outputArea()).toBeHidden();
 		await expect(fillAndSubmitPage.redBorderEmailField()).toBeVisible();
